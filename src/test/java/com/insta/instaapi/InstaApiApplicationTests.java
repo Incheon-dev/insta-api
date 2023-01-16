476b5902eea6f804e4dfa06cfd947672bc3aa3ff
ChoiDevv <oinkoink71@naver.com>
2023-01-16 14:14:19 +0900

    Initial commit

---
+++ src/test/java/com/insta/instaapi/InstaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insta.instaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
