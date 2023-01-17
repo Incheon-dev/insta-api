f8befcfa6858493baaaa3f7d508dc3b1c176e30b
ChoiDevv <oinkoink71@naver.com>
2023-01-17 15:16:17 +0900

    [추가] swagger2 적용

---
+++ src/main/java/com/insta/instaapi/check/CheckController.java
@@ -0,0 +1,14 @@
+package com.insta.instaapi.check;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CheckController {
+
+    @RequestMapping(value = "/api/v1/check", method = RequestMethod.GET)
+    public String check() {
+        return "check";
+    }
+}
