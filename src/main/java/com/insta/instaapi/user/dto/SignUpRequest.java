d74cac053461b618ecccb7b26624efbb847b0741
ChoiDevv <oinkoink71@naver.com>
2023-01-17 20:34:42 +0900

    [추가] 임시 api 단위 테스트 추가

--- src/main/java/com/insta/instaapi/user/dto/SignUpRequest.java
+++ src/main/java/com/insta/instaapi/user/dto/SignUpRequest.java
@@ -1,5 +1,6 @@
 package com.insta.instaapi.user.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,4 +10,10 @@ public class SignUpRequest {
 
     private String username;
     private String password;
+
+    @Builder
+    public SignUpRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
