3aac6ee8fa6abb501e639b90966b4d09e1699be3
ChoiDevv <oinkoink71@naver.com>
2023-01-22 02:51:32 +0900

    [추가] 유저의 존재 여부에 대한 검증 테스트 코드 추가, 향후에 Mock 객체를 이용해서 리팩토링 예정.

--- src/test/java/com/insta/instaapi/user/controller/UserControllerTest.java
+++ src/test/java/com/insta/instaapi/user/controller/UserControllerTest.java
@@ -1,16 +1,17 @@
 package com.insta.instaapi.user.controller;
 
 import com.insta.instaapi.user.dto.request.SignUpRequest;
+import com.insta.instaapi.user.dto.request.UpdatePasswordRequest;
 import com.insta.instaapi.user.entity.Users;
 import com.insta.instaapi.user.entity.repository.UserRepository;
 import org.junit.After;
 import org.junit.Test;
-import org.junit.jupiter.api.Disabled;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -18,7 +19,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.in;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -113,5 +113,37 @@ public class UserControllerTest {
 
         //then
         assertThat(responseEntity.getBody()).isEqualTo("해당 번호로 가입된 유저가 존재합니다.");
+    }
+
+    @Test
+    public void 유저가_존재하는지_검증한다() throws Exception {
+        //given
+        String phoneNumber = "phoneNumber1";
+        String email = "email1";
+        String name = "name1";
+        String nickname = "nickname1";
+        String password = "{BCrypt}password1";
+        String introduction = "introduction1";
+        String sex = "sex1";
+
+        Users user = Users.builder()
+                .phoneNumber(phoneNumber)
+                .email(email)
+                .name(name)
+                .nickname(nickname)
+                .password(password)
+                .introduction(introduction)
+                .sex(sex)
+                .build();
+
+        userRepository.save(user);
+
+        String url = "http://localhost:" + port + "/api/account?email={email}";
+
+        //when
+        Boolean response = restTemplate.getForObject(url, Boolean.class, email);
+
+        //then
+        assertThat(response).isEqualTo(true);
     }
 }
